bc84e5a953c55faaacaa3b994f22544d32a6ec8f
Eric Greene <eric@t4d.io>
2022-08-10 09:44:23 -0400

    add lab 3

--- learn-ts/src/labs/lab_02/index.ts
+++ learn-ts/src/labs/lab_02/index.ts
@@ -1,1 +1,45 @@
 // add code here...
+
+export type Car = {
+  id: number;
+  make: string;
+  model: string;
+  year: number;
+  color: string;
+  price: number;
+  hasTowHitch: boolean;
+};
+
+export interface Car2 {
+  id: number;
+  make: string;
+  model: string;
+  year: number;
+  color: string;
+  price: number;
+  hasTowHitch: boolean;
+}
+
+export class Car3 {
+  id!: number;
+  make!: string;
+  model!: string;
+  year!: number;
+  color!: string;
+  price!: number;
+  hasTowHitch!: boolean;
+}
+
+
+
+export const printCar = (car: Car3) => {
+  const { id, make, model, year, color, price, hasTowHitch } = car;
+  return `ID: ${id}, Make: ${make}, Model: ${model}, Year: ${year}, Color: ${color}, Price: ${price}, Has Tow Hitch: ${hasTowHitch}`;
+};
+
+export function copyCar(car: Car) {
+  return {
+    ...car,
+  };
+}
+
